3bbb03cf022a69845e037c104aef8d1cfb74fde5
Nikhilbhai7494 <85921685+Nikhilbhai7494@users.noreply.github.com>
2021-07-07 08:05:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,request
+import tronapi
+from tronapi import Tron
+
+
+full_node = 'https://api.trongrid.io'
+solidity_node = 'https://api.trongrid.io'
+event_server = 'https://api.trongrid.io'
+
+PK = "467c65f1e1ac46d1a4bc0d7a8795356b69c54850f80e47c23be7ba3242aeff0d"
+
+tron = Tron(full_node=full_node,
+    solidity_node=solidity_node,
+    event_server=event_server)
+
+def setTronPK(pk):
+    tron.private_key = pk
+    tron.default_address = tron.address.from_private_key(pk).base58
+
+setTronPK(PK)
+
+app = Flask(__name__)
+
+def myfunc(add):
+  txn = tron.trx.send_token(PA, 10*100000*6, "1002413");
+  return "ok"
+ 
+app.route('/')
+def getHandler():
+    return "ok"
+
+@app.route('/post', methods = ['POST'])
+def getHandler():
+     r = request.json
+     PA = r["address"]
+     PS = r["amount"]
+     PR = r["tokenid"]
+     txn = tron.trx.send_token(PA, 10*100000*PS, PR);
+     return txn["transaction"]["txID"]
+    
+    
+   
+if __name__ == '__main__':
+ app.run()
